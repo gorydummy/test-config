9552e4a22786050fc579e184907e47b8aa7c8223
Gory Dummy <gorydummy@gmail.com>
2018-11-04 12:44:54 +0800

    added init

--- user.rb
+++ user.rb
@@ -1,3 +1,8 @@
 class User
 	attr_reader :full_name, :first_name, :last_name, :age, :hobbies
+
+	def initialize(first_name, last_name)
+		@first_name = first_name
+		@last_name = last_name
+	end
 end
